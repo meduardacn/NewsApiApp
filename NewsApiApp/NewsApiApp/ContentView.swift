b532400689ed4e44f5af871aa17e419d73b5d421
meduardcn <m.eduardacn@gmail.com>
2020-07-28 12:05:10 -0300

    first commit

---
+++ NewsApiApp/NewsApiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApiApp
+//
+//  Created by Maria Eduarda Casanova Nascimento on 28/07/20.
+//  Copyright © 2020 Maria Eduarda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
